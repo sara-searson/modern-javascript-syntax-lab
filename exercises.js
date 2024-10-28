c7c33df492802f21b3702ec6ec8a022c991bef3a
sara-searson <sara.r.searson@gmail.com>
2024-10-28 09:31:11 -0400

    complete exercise 4

--- exercises.js
+++ exercises.js
@@ -2,41 +2,53 @@
 
 // Use `.map()` to iterate over the following array:
 
-const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+// const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 
 // Create a new array where each value is multiplied by 2 and log the new array.
 
 // Your code here
 
-const doubled = nums.map((number) => {
-    return number * 2
-})
-console.log(doubled)
+// const doubled = nums.map((number) => {
+//     return number * 2
+// })
+// console.log(doubled)
 
 //-----Exercise 2-----
 
 // Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
 
-const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
 // Your code here
 
-const [firstTopping, secondTopping] = pizzaToppings
+// const [firstTopping, secondTopping] = pizzaToppings
 
-console.log(firstTopping)
-console.log(secondTopping)
+// console.log(firstTopping)
+// console.log(secondTopping)
 
 //-----Exercise 3-----
 
 // Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
 
-const car = {
-    make: 'Audi',
-    model: 'q5',
-  };
+// const car = {
+//     make: 'Audi',
+//     model: 'q5',
+// };
   
-  // Your code here
-const {make, model} = car
+// Your code here
+// const {make, model} = car
 
-console.log(make)
-console.log(model)  
+// console.log(make)
+// console.log(model)  
+
+//-----Exercise 4-----
+
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+
+const controversialPizzaToppings = [...pizzaToppings]
+
+console.log(controversialPizzaToppings)
