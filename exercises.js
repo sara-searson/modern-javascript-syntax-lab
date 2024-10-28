1b4d7f96ebcac8ee90a3252f0a7d288d97f30d31
sara-searson <sara.r.searson@gmail.com>
2024-10-28 09:58:46 -0400

    complete exercise 10

--- exercises.js
+++ exercises.js
@@ -121,14 +121,49 @@
 
 // Convert the following `if...else` statement in to a ternary:
 
-let pizza = 'tasty';
+// let pizza = 'tasty';
 
-if (pizza === 'tasty') {
-  console.log('yum');
-} else {
-  console.log('yuck');
-}
+// if (pizza === 'tasty') {
+//   console.log('yum');
+// } else {
+//   console.log('yuck');
+// }
 
 // Your code here
 
-pizza === 'tasty' ? console.log('yum') : console.log('yuck');
+// pizza === 'tasty' ? console.log('yum') : console.log('yuck');
+
+
+//-----Exercise 10-----
+
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = null;  // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+
+const LANG = localLangConfig || 'en'
+
+// Log the result
+console.log('Language setting:', LANG);
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = 'dark'; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+
+const USER_THEME = userSavedTheme || 'light'
+
+// Log the result
+console.log('User theme setting:', USER_THEME);
