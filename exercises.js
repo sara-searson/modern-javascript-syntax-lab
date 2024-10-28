3d3bfc6bc036bee66f367a010b382a33afe09777
sara-searson <sara.r.searson@gmail.com>
2024-10-28 09:35:17 -0400

    complete exercise 5

--- exercises.js
+++ exercises.js
@@ -13,6 +13,7 @@
 // })
 // console.log(doubled)
 
+
 //-----Exercise 2-----
 
 // Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
@@ -25,6 +26,7 @@
 
 // console.log(firstTopping)
 // console.log(secondTopping)
+
 
 //-----Exercise 3-----
 
@@ -41,14 +43,36 @@
 // console.log(make)
 // console.log(model)  
 
+
 //-----Exercise 4-----
 
 // Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
 
-const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+// const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
 
 // Your code here
 
-const controversialPizzaToppings = [...pizzaToppings]
+// const controversialPizzaToppings = [...pizzaToppings]
 
-console.log(controversialPizzaToppings)
+// console.log(controversialPizzaToppings)
+
+
+//-----Exercise 5-----
+
+// Duplicate the following object and spread its values into a new variable `myCar`.
+
+const car = {
+    make: 'Audi',
+    model: 'q5',
+};
+  
+// Change the `model` property of `myCar` to 'q7'. Log both objects.
+
+// Your code here
+  
+const myCar = {...car}
+
+myCar.model = 'q7'
+
+console.log(car)
+console.log(myCar)
